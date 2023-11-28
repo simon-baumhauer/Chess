cbe2f232b241696339b6a620e677ba4cc2e0eafb
simon <simon@SIMONSLAPTOP>
2023-11-28 06:53:02 +0100

    added chess pieces

---
+++ script.js
@@ -0,0 +1,6 @@
+const gameboard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+const startPieces = [];
+
